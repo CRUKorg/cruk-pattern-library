02c93b08036d29fb2368f980b86a797a2a41d996
heylookalive <allister.price@gmail.com>
2016-05-17 14:01:18 +0100

    Crappy stopgap change to make code examples less broken.

--- docs/assets/js/docs.js
+++ docs/assets/js/docs.js
@@ -10,10 +10,10 @@ jQuery(function() {
       $(this).html('');
 
       var full_html = '<!DOCTYPE html><html lang="en"><head><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="' + document.location.origin + '/cruk-pattern-library/assets/css/docs.css"><script src="https://use.typekit.net/bgw6lhg.js"></script><script>try{Typekit.load({ async: true });}catch(e){}</script></head><body class="example">' + html + '</body></html>';
-      var $iframe = $('<iframe class="cr-pattern-library-code-example" height="100%" />').appendTo($(this));
+      var $iframe = $('<iframe class="cr-pattern-library-code-example" width="1024" height="100%" />').appendTo($(this));
       //$iframe.src = "javascript:'"+full_html+"'";
       $iframe[0].contentWindow.document.write(full_html);
-      $iframe.attr('width', '1024px').attr('height', $iframe[0].contentWindow.document.body.scrollHeight + 'px');
+      $iframe.attr('height', $iframe[0].contentWindow.document.body.scrollHeight + 'px');
     });
   }
 });
