b3bcc743d76ba99dcf0a7affbb96984b97abe643
Dan Burzynski <contax@phrenetic.org>
2016-07-11 12:16:26 +0100

    Fixed incorrect selector on ladda-button

--- assets/js/cruk-base/plugin-ladda.js
+++ assets/js/cruk-base/plugin-ladda.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function(){
-    var selector = 'submit.ladda-button';
+    var selector = 'button.ladda-button';
     Ladda.bind(selector, { timeout: 60000 });
 
     jQuery(selector).parents('form').delegate('input, select, textarea', 'clientsideValidationInvalid', function (e) {
