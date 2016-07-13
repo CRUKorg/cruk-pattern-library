d28c3d767cff1455142e60a234e079fcda43793c
Dan Burzynski <contax@phrenetic.org>
2016-07-13 15:10:36 +0100

    ER-1363 - By having the plugin keep a track of the state of fields, we can dramatically improve its performance, which is vital when the clientside validation code re-evaluates all fields when the submit button is pressed.

--- assets/js/cruk-base/plugin-cruk-dynamic-input.js
+++ assets/js/cruk-base/plugin-cruk-dynamic-input.js
@@ -9,57 +9,88 @@
  *
  */
 (function ($) {
+    var dataKey = 'crukInputDynamicState';
+
     $.fn.crukInputDynamicClear = function crukInputDynamicClear() {
-        $(this).crukInputDynamicSpin(false);
-        $(this).crukInputDynamicSuccess(false);
+        return this.each(function() {
+            if ($(this).crukInputDynamicState() !== null) {
+                $(this).crukInputDynamicSpin(false);
+                $(this).crukInputDynamicSuccess(false);
+            }
+        });
     };
 
     $.fn.crukInputDynamicSuccess = function crukInputDynamicSuccess(remove) {
         return this.each(function() {
+            var stateValue = 'success';
+
             if (remove === false) {
-                $(this).siblings('span.form-control-feedback').removeClass('fa').removeClass('fa-check');
-                $(this).siblings('span.sr-only').text('');
+                if ($(this).crukInputDynamicState() === stateValue) {
+                    $(this).siblings('span.form-control-feedback').removeClass('fa').removeClass('fa-check');
+                    $(this).siblings('span.sr-only').text('');
+                    $(this).data(dataKey, null);
+                }
             } else {
-                // It may well be that we need to clear a throbber... so, just in case...
-                $(this).crukInputDynamicClear();
+                if ($(this).crukInputDynamicState() !== stateValue) {
+                    // It may well be that we need to clear a throbber... so, just in case...
+                    $(this).crukInputDynamicClear();
 
-                // Now tick away!
+                    // Now tick away!
 
-                $(this).siblings('span.form-control-feedback').addClass('fa').addClass('fa-check');
-                $(this).siblings('span.sr-only').text('(success)');
+                    $(this).siblings('span.form-control-feedback').addClass('fa').addClass('fa-check');
+                    $(this).siblings('span.sr-only').text('(success)');
+                    $(this).data(dataKey, stateValue);
+                }
             }
         });
     };
 
     $.fn.crukInputDynamicSpin = function crukInputDynamicSpin(remove) {
         return this.each(function() {
-            var height = $(this).outerHeight();
-            if (height > 32) {
-                height *= 0.8;
-            }
-            var lines = 12, radius = height * 0.2, length = radius * 0.6, width = radius < 7 ? 2 : 3;
-            var opts = {
-                color: "#000",
-                lines: lines,
-                radius: radius,
-                length: length,
-                width: width,
-                zIndex: "auto",
-                top: "auto",
-                left: "auto",
-                className: ""
-            };
+            var stateValue = 'checking';
 
             if (remove === false) {
-                $(this).siblings('span.form-control-feedback').spin(false);
-                $(this).siblings('span.sr-only').text('');
+                if ($(this).crukInputDynamicState() === stateValue) {
+                    $(this).siblings('span.form-control-feedback').spin(false);
+                    $(this).siblings('span.sr-only').text('');
+                    $(this).data(dataKey, null);
+                }
             } else {
-                // Remove any ticks we may have
-                $(this).crukInputDynamicClear();
+                if ($(this).crukInputDynamicState() !== stateValue) {
+                    // Remove any ticks we may have
+                    $(this).crukInputDynamicClear();
 
-                $(this).siblings('span.form-control-feedback').spin(opts);
-                $(this).siblings('span.sr-only').text('(checking)');
+                    var height = $(this).outerHeight();
+                    if (height > 32) {
+                        height *= 0.8;
+                    }
+                    var lines = 12, radius = height * 0.2, length = radius * 0.6, width = radius < 7 ? 2 : 3;
+                    var opts = {
+                        color: "#000",
+                        lines: lines,
+                        radius: radius,
+                        length: length,
+                        width: width,
+                        zIndex: "auto",
+                        top: "auto",
+                        left: "auto",
+                        className: ""
+                    };
+
+                    $(this).siblings('span.form-control-feedback').spin(opts);
+                    $(this).siblings('span.sr-only').text('(checking)');
+                    $(this).data(dataKey, stateValue);
+                }
             }
         });
+    };
+
+    $.fn.crukInputDynamicState = function() {
+        var state = $(this).data(dataKey);
+        if (typeof state === 'undefined') {
+            state = null;
+        }
+
+        return state;
     };
 })(jQuery);
