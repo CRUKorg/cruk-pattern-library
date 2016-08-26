4a90a48220e14e275a8f98b28962f93c8c506eaa
David Hughes <david.hughes125@gmail.com>
2016-08-26 13:06:16 +0100

    Rename 'Accordion' pattern to 'Collapsible Fieldset'.

--- assets/js/cruk-base/plugin-accordion.js
+++ assets/js/cruk-base/plugin-accordion.js
@@ -12,13 +12,13 @@ jQuery( document ).ready(function( $ ) {
         }
     };
 
-    $('.accordion').on('show.bs.collapse', function (e) {
+    $('.collapsible-fieldset').on('show.bs.collapse', function (e) {
         var target = $(e.currentTarget);
         target.find('i.' + states.hide.icon).addClass(states.show.icon).removeClass(states.hide.icon);
         target.find('.add-details').text(states.show.text);
     });
 
-    $('.accordion').on('hide.bs.collapse', function (e) {
+    $('.collapsible-fieldset').on('hide.bs.collapse', function (e) {
         var target = $(e.currentTarget);
         target.find('i.' + states.show.icon).addClass(states.hide.icon).removeClass(states.show.icon);
         target.find('.add-details').text(states.hide.text);
--- docs/collapsible-fieldset.md
+++ docs/collapsible-fieldset.md
@@ -1,6 +1,6 @@
 ---
 layout: page
-title: Accordion
+title: Collapsible Fieldset
 ---
 
 DOCUMENTATION TO FOLLOW. SORRY!
@@ -8,7 +8,7 @@ DOCUMENTATION TO FOLLOW. SORRY!
 ## Contents
 
 {% example html %}
-<div class="accordion collapsible panel panel-default">
+<div class="collapsible-fieldset collapsible panel panel-default">
   <legend class="panel-heading">
           <a href="#" class="panel-title" data-toggle="collapse" data-target="#edit-1 > .collapse" aria-expanded="true"><span class="fieldset-legend-prefix element-invisible">Show</span><h2><i class="fa fa-chevron-circle-down" aria-hidden="true"></i>Additional participant 1<span class="pull-right add-details">Add details</span></h2></a>
   </legend>
